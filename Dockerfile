89735044a91b9f1043fcbfb8c5e5101c67f2102a
Jean-Christophe Hoelt <hoelt@fovea.cc>
2014-12-06 07:26:00 +0000

    Install wine jdk from air user

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM nitvotlu/wine
 COPY docker-init.sh /docker-init.sh
 RUN /docker-init.sh
-USER air
+CMD su - air
--- docker-init.sh
+++ docker-init.sh
@@ -12,10 +12,19 @@ echo 'export WINEDEBUG=fixme-all' >> ~air/.bashrc
 mkdir -p /opt/jdk
 cd /opt/jdk
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u71-b14/jdk-7u71-linux-x64.tar.gz
-wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u71-b14/jdk-7u71-windows-i586.exe
 tar xvzf jdk-7u71-linux-x64.tar.gz 
+rm /opt/jdk/jdk-7u71-linux-x64.tar.gz
+
 echo 'export JAVA_HOME=/opt/jdk/jdk1.7.0_71' >> ~air/.bashrc 
 echo 'export PATH="$JAVA_HOME/bin:$PATH"' >> ~air/.bashrc 
 
-su - air -c "wine /opt/jdk/jdk-7u71-windows-i586.exe"
-rm /opt/jdk/jdk-7u71-linux-x64.tar.gz /opt/jdk/jdk-7u71-windows-i586.exe 
+cat << EOF > /.docker-init-win-jdk.sh
+cd /home/air
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u71-b14/jdk-7u71-windows-i586.exe
+wine jdk-7u71-windows-i586.exe
+sleep 1
+rm /opt/jdk/jdk-7u71-windows-i586.exe 
+EOF
+chmod +x /docker-init-win-jdk.sh
+
+su -c air /docker-init-win-jdk.sh
